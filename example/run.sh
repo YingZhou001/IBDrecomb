e879da4afc9ec713214670292c10a9c7a1bd4602
YingZhou001 <31318632+YingZhou001@users.noreply.github.com>
2019-08-15 17:37:12 -0700

    Add files via upload

--- example/run.sh
+++ example/run.sh
@@ -1,5 +1,5 @@
-../src/IBDrecomb -refinedibd test.ibd.gz -fbin 10000 > test.10000.map
+../src/IBDrecomb -refinedibd test.1.ibd.gz -fbin 10000 > test.10000.map
 
 ../src/IBDrecomb -ibd test.2.ibd.gz -fbin 10000 > test.10000.map
 
-../src/IBDrecomb -refinedibd test.ibd.gz -fbin 10000 -rate 1.21 > test.10000.normalized.map
+../src/IBDrecomb -refinedibd test.1.ibd.gz -fbin 10000 -rate 1.21 > test.10000.normalized.map
